aac4abc37b21719e5c49789d377fc8e28c65e6c9
valeria211-ui <valeria.panchina@yandex.ru>
2023-05-24 03:36:18 +0700

    adding input

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,40 @@ int main()
 	int m[100000];
 	long long choice, len;
 	double time;
+
+	while (true)
+	{
+  		cout << "Select the sort type:\n(0) exit\n(1) Sorting by choice\n(2) BubbleSort\n(3) InsertSort\n(4) MergeSort\n(5) QuickSort\n";
+  		cin >> choice;
+  		if (cin.fail() || choice < 0 || choice > 5)
+  		{
+    		cout << "Invalid input. Please enter a number between 0 and 5\n";
+    		cin.clear();
+    		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  		}
+  		else
+  		{
+    		break;
+  		}
+	}
+
+	while (true)
+	{
+  		cout << "Write the length of the array under test:\n";
+  		cin >> len;
+  		if (cin.fail() || len < 1 || len > 100000)
+  	{
+    	cout << "Invalid input. Please enter a number between 1 and 100000\n";
+    	cin.clear();
+    	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  	}
+  		else
+  		{
+    		break;
+  		}
+	}
+
+
 	while (true)
 	{
 		cout << "Select the sort type:\n(0) exit\n(1) Sorting by choice\n(2) BubbleSort\n(3) InsertSort\n(4) MergeSort\n(5) QuickSort\n";
